6a236f30ecf852502166489011b7fb7eaffaf437
Igor Suchilov <igor.suchilov@gmail.com>
2022-10-07 01:03:34 +0200

    Made camera button work

--- app/src/main/java/com/example/cloudfacecounter/MainActivity.kt
+++ app/src/main/java/com/example/cloudfacecounter/MainActivity.kt
@@ -1,160 +1,41 @@
 package com.example.cloudfacecounter
-//
-//import android.content.Context
-//import android.os.Bundle
-//import com.google.android.material.snackbar.Snackbar
-//import androidx.appcompat.app.AppCompatActivity
-//import androidx.navigation.findNavController
-//import androidx.navigation.ui.AppBarConfiguration
-//import androidx.navigation.ui.navigateUp
-//import androidx.navigation.ui.setupActionBarWithNavController
-//import android.view.Menu
-//import android.view.MenuItem
-//import com.example.cloudfacecounter.databinding.ActivityMainBinding
-//import com.karumi.dexter.Dexter
-//import com.karumi.dexter.PermissionToken
-//import com.karumi.dexter.listener.PermissionDeniedResponse
-//import com.karumi.dexter.listener.PermissionGrantedResponse
-//import com.karumi.dexter.listener.PermissionRequest
-//import com.karumi.dexter.listener.single.PermissionListener
-//
-//class MainActivity : AppCompatActivity() {
-//
-//    private lateinit var appBarConfiguration: AppBarConfiguration
-//private lateinit var binding: ActivityMainBinding
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//     binding = ActivityMainBinding.inflate(layoutInflater)
-//     setContentView(binding.root)
-//
-//        setSupportActionBar(binding.toolbar)
-//
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//
-//        binding.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show()
-//        }
-//    }
-//override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-//        return true
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        return when(item.itemId) {
-//            R.id.action_settings -> true
-//            else -> super.onOptionsItemSelected(item)
-//        }
-//    }
-//
-//    override fun onSupportNavigateUp(): Boolean {
-//    val navController = findNavController(R.id.nav_host_fragment_content_main)
-//    return navController.navigateUp(appBarConfiguration)
-//            || super.onSupportNavigateUp()
-//    }
-//}
 
-//import android.R
-import android.Manifest
-import android.app.AlertDialog
+import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.provider.Settings
-import android.widget.Button
-import android.widget.Toast
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import android.os.Bundle
+import android.provider.MediaStore
+import com.example.cloudfacecounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // initializing our button and adding on click listener to it.
-        val openGalleryButton = findViewById<Button>(R.id.idBtnOpenGallery)
-        openGalleryButton.setOnClickListener {
-            // inside on click listener calling method to request permission
-            requestPermissions()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.btnCamera.setOnClickListener {
+            dispatchTakePictureIntent()
         }
+
+    }
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+
     }
 
-    private fun requestPermissions() {
-        // below line is use to request permission in the current activity.
-        // this method is use to handle error in runtime permissions
-        Dexter.withActivity(this)
-            // below line is use to request the number of permissions which are required in our app.
-            .withPermissions(Manifest.permission.CAMERA,
-                // below is the list of permissions
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.READ_CONTACTS)
-            // after adding permissions we are calling an with listener method.
-            .withListener(object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(multiplePermissionsReport: MultiplePermissionsReport) {
-                    // this method is called when all permissions are granted
-                    if (multiplePermissionsReport.areAllPermissionsGranted()) {
-                        // do you work now
-                        Toast.makeText(this@MainActivity, "All the permissions are granted..", Toast.LENGTH_SHORT).show()
-                    }
-                    // check for permanent denial of any permission
-                    if (multiplePermissionsReport.isAnyPermissionPermanentlyDenied) {
-                        // permission is denied permanently, we will show user a dialog message.
-                        showSettingsDialog()
-                    }
-                }
-
-                override fun onPermissionRationaleShouldBeShown(list: List<PermissionRequest>, permissionToken: PermissionToken) {
-                    // this method is called when user grants some permission and denies some of them.
-                    permissionToken.continuePermissionRequest()
-                }
-            }).withErrorListener {
-                // we are displaying a toast message for error message.
-                Toast.makeText(applicationContext, "Error occurred! ", Toast.LENGTH_SHORT).show()
-            }
-            // below line is use to run the permissions on same thread and to check the permissions
-            .onSameThread().check()
-    }
-
-    // below is the shoe setting dialog method
-    // which is use to display a dialogue message.
-    private fun showSettingsDialog() {
-        // we are displaying an alert dialog for permissions
-        val builder = AlertDialog.Builder(this@MainActivity)
-
-        // below line is the title for our alert dialog.
-        builder.setTitle("Need Permissions")
-
-        // below line is our message for our dialog
-        builder.setMessage("This app needs permission to use this feature. You can grant them in app settings.")
-        builder.setPositiveButton("GOTO SETTINGS") { dialog, which ->
-            // this method is called on click on positive button and on clicking shit button
-            // we are redirecting our user from our app to the settings page of our app.
-            dialog.cancel()
-            // below is the intent from which we are redirecting our user.
-            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-            val uri = Uri.fromParts("package", packageName, null)
-            intent.data = uri
-            startActivityForResult(intent, 101)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imgViewer.setImageBitmap(imageBitmap)
         }
-        builder.setNegativeButton("Cancel") { dialog, which ->
-            // this method is called when user click on negative button.
-            dialog.cancel()
-        }
-        // below line is used to display our dialog
-        builder.show()
     }
 }
